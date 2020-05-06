4e5bb3e1be4faa900a0b4be990489b1de3a48e23
Rob <rob.toloy@naturetrak.com>
2020-05-06 13:51:49 -0700

    Add modals to Grid products.

--- src/Components/Grid.js
+++ src/Components/Grid.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import Modal from 'react-modal';
+import placeholder from '.././Assets/placeholder-img.svg';
 
 import g1 from '../Assets/g1.png';
 import g2 from '../Assets/g2.png';
@@ -9,7 +11,37 @@ import g6 from '../Assets/g6.png';
 import g7 from '../Assets/g7.png';
 import g8 from '../Assets/g8.png';
 
+const gridModalStyles = {
+  content : {
+    top                   : '50%',
+    left                  : '50%',
+    right                 : 'auto',
+    bottom                : 'auto',
+    marginRight           : '-50%',
+    transform             : 'translate(-50%, -50%)',
+    width                 : '65%',
+    border                : 'none'
+  }
+};
+
+Modal.setAppElement('#root')
+
 const grid = () => {
+  var subtitle;
+  const [modalIsOpen,setIsOpen] = React.useState(false);
+  function openGridModal() {
+    setIsOpen(true);
+  }
+
+  function afterGridOpenModal() {
+    // references are now sync'd and can be accessed.
+    // subtitle.style.color = '#f00';
+  }
+
+  function closeGridModal(){
+    setIsOpen(false);
+  }
+
   return (
     <div className="Grid-container">
       <div className="Grid-section-text mb-48">
@@ -17,52 +49,76 @@ const grid = () => {
       </div>
 
       <div className="Grid-row">
-        <div className="Grid-item">
+        <button onClick={openGridModal} className="Grid-item">
           <img className="mb-12" src={g1} alt="product"/>
           <p>Product Name</p>
           <p className="grid-price">$18.00</p>
-        </div>
-        <div className="Grid-item">
+        </button>
+        <button onClick={openGridModal} className="Grid-item">
           <img className="mb-12" src={g2} alt="product"/>
           <p>Product Name</p>
           <p className="grid-price">$28.00</p>
-        </div>
-        <div className="Grid-item">
+        </button>
+        <button onClick={openGridModal} className="Grid-item">
           <img className="mb-12" src={g3} alt="product"/>
           <p>Product Name</p>
           <p className="grid-price">$38.00</p>
-        </div>
-        <div className="Grid-item">
+        </button>
+        <button onClick={openGridModal} className="Grid-item">
           <img className="mb-12" src={g4} alt="product"/>
           <p>Product Name</p>
           <p className="grid-price">$48.00</p>
-        </div>
+        </button>
       </div>
 
       <div className="mb-12"></div>
 
       <div className="Grid-row">
-        <div className="Grid-item">
+        <button onClick={openGridModal} className="Grid-item">
           <img className="mb-12" src={g5} alt="product"/>
           <p>Product Name</p>
           <p className="grid-price">$58.00</p>
-        </div>
-        <div className="Grid-item">
+        </button>
+        <button onClick={openGridModal} className="Grid-item">
           <img className="mb-12" src={g6} alt="product"/>
           <p>Product Name</p>
           <p className="grid-price">$68.00</p>
-        </div>
-        <div className="Grid-item">
+        </button>
+        <button onClick={openGridModal} className="Grid-item">
           <img className="mb-12" src={g7} alt="product"/>
           <p>Product Name</p>
           <p className="grid-price">$78.00</p>
-        </div>
-        <div className="Grid-item">
+        </button>
+        <button onClick={openGridModal} className="Grid-item">
           <img className="mb-12" src={g8} alt="product"/>
           <p>Product Name</p>
           <p className="grid-price">$88.00</p>
-        </div>
+        </button>
       </div>
+
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterGridOpenModal}
+        onRequestClose={closeGridModal}
+        style={gridModalStyles}
+        contentLabel="Featured Product Modal"
+        closeTimeoutMS={500}
+      >
+        <div className="Modal-container">
+          <div className="Modal-row">
+            <div className="Modal-col">
+              <img src={placeholder} width="450" className="Modal-img" />
+            </div>
+            <div className="Modal-col ml-24">
+              <h2 ref={_subtitle => (subtitle = _subtitle)}>Grid Product Name</h2>
+              <p className="mb-24">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniamquis.</p>
+              <h3 className="mb-12">$19.99  <strike className="grey">$39.99</strike></h3>
+              <button className="Btn">Get deal</button>
+            </div>
+          </div>
+          <div className="Modal-close" onClick={closeGridModal}></div>
+        </div>
+      </Modal>
     </div>
   );
 }
